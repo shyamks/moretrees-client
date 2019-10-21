c9d8ad1246431381b8cb2305d3c0a3fad79ed362
Shyam Kodmad <shyam.kodmad@gmail.com>
2019-10-21 17:31:29 +0530

    added Page

---
+++ src/Pages/Volunteer.js
@@ -0,0 +1,20 @@
+import Header from '../components/Header'
+import VolunteerChoices from '../components/volunteerChoices'
+import React from 'react'
+import styled from 'styled-components'
+import Footer from '../components/Footer';
+import { PageContent, Page } from '../constants';
+
+export function Volunteer() {
+    return (
+        <>
+            <Header />
+            <Page>
+                <PageContent>
+                    <VolunteerChoices />
+                </PageContent>
+                <Footer />
+            </Page>
+        </>
+    )
+}
