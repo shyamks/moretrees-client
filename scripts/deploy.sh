375a481d716d3e9779a4723e800e6830a0486395
Shyam Kodmad <shyam.kodmad@gmail.com>
2019-11-03 22:46:13 +0530

    check process, deploy command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,9 @@
 cd moretrees-client
+
+for pid in `ps aux | grep [s]erver.js | awk '{print $2}'` ; do kill -9 $pid ; done
+
+echo "After kill command, node processes => "
+ps aux | grep '[s]erver.js'
+
 npm run build
 npm run start:prod
